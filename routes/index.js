aa59634161aad1ecc73d20c5af72b41397f00ead
ronald-luo <r.luo.180@gmail.com>
2021-09-18 15:58:05 -0400

    🖼️ update: create wireframes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('landing', { title: 'Express' });
 });
+
+/* GET home page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Express' });
+});
+
+
 
 module.exports = router;
